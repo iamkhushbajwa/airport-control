b74968a54f99983eafd8df863db6978219db5b8d
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-03-07 13:02:50 +0000

    Ability to park planes at the airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,8 +1,13 @@
 DEFAULT_CAPACITY = 100
 class Airport
-  attr_reader :capacity
+  attr_reader :capacity, :planes
   def initialize(options={})
     @capacity = options[:capacity] || DEFAULT_CAPACITY
+    @planes = []
+  end
+
+  def park(plane)
+    @planes << plane
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -12,5 +12,13 @@ describe Airport do
       airport_two = Airport.new({:capacity => 50})
       expect(airport_two.capacity).to eq(50)
     end
+
+    it "can park planes" do
+      airport = Airport.new
+      plane = double :plane
+      airport.park(plane)
+      expect(airport.planes).to eq([plane])
+    end
+
   end
 end
