cd9af30becefc0d5cd85615e145802829e5af347
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-03-07 17:48:18 +0000

    Refactored tests

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -17,39 +17,36 @@ describe Airport do
   end
 
   context "Standard Traffic control" do
-    it "can have capacity" do
+    before(:each) do
       set_weather_to("Sunny")
+    end
+    it "can have capacity" do
       expect(airport.capacity).to eq(100)
     end
 
     it "can set capacity" do
-      set_weather_to("Sunny")
-      airport_two = Airport.new({:capacity => 50})
-      expect(airport_two.capacity).to eq(50)
+      another_airport = Airport.new({:capacity => 50})
+      expect(another_airport.capacity).to eq(50)
     end
 
     it "can allow landing of planes" do
-      set_weather_to("Sunny")
       expect(flying_plane).to receive(:landing)
       airport.allow_landing(flying_plane)
       expect(airport.planes).to eq([flying_plane])
     end
 
     it "when it is full it will not allow landing" do
-      set_weather_to("Sunny")
       fill_airport
       expect(airport.full?).to be_true
       expect(lambda {airport.allow_landing(flying_plane)}).to raise_error(RuntimeError)
     end
 
     it "when it is empty will not allow take-off" do
-      set_weather_to("Sunny")
       expect(airport.empty?).to be_true
       expect(lambda {airport.allow_take_off(grounded_plane)}).to raise_error(RuntimeError)
     end
 
     it "ensure that only flying planes can be landed" do
-      set_weather_to("Sunny")
       expect(flying_plane).to receive(:landing)
       airport.allow_landing(flying_plane)
       expect(airport.planes).to eq([flying_plane])
@@ -68,8 +65,6 @@ describe Airport do
   end
 
   context "Extreme Weather Traffic Control" do
-
-
     it "won't allow take off if weather is stormy" do
       set_weather_to("Sunny")
       expect(flying_plane).to receive(:landing)
