fa27598694281192d7baa6fa165126be7b4549d8
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-03-07 13:17:55 +0000

    Refactored code to ensure planes cannot land if full AND take-off if empty

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -27,13 +27,17 @@ describe Airport do
       expect(airport.planes).to eq([plane])
     end
 
-    it "knows when it is full" do
+    it "when it is full it will not allow landing" do
       fill_airport
       expect(airport.full?).to be_true
+      plane = double :plane
+      expect(lambda {airport.allow_landing(plane)}).to raise_error(RuntimeError)
     end
 
-    it "knows when it is empty" do
+    it "when it is empty will not allow take-off" do
       expect(airport.empty?).to be_true
+      plane = double :plane
+      expect(lambda {airport.allow_take_off(plane)}).to raise_error(RuntimeError)
     end
   end
 end
